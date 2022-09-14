bb39de1d2d76a0189c65950f945ce11af265968d
eyobs <ttoeyob@gmail.com>
2022-09-14 15:38:51 +0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log("Microservices rock!");
     await sleep(5000);
   }
 }
